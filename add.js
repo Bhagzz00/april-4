1f308effa84d9b6130d3c308ab5a0b59d8b9004c
Bhagyashri Felix Rodrigues <743317@student.tor.loc>
2019-04-04 10:57:37 -0400

    commit

---
+++ add.js
@@ -0,0 +1,13 @@
+
+console.log ("The numbers are :")
+for (var i=1; i<=11; i++){
+    console.log(i)
+}
+var sum =0;
+var num =1;
+while(num <=11)
+{
+    sum=sum+num;
+    num++;
+}
+console.log("The sum of the numbers is"+sum)
