ad08ed24266163ef359d0d906eafca9036ef007c
Bhagyashri Felix Rodrigues <743317@student.tor.loc>
2019-04-04 09:31:50 -0400

    secondcommit

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 var x, y, z;          // How to declare variables
-x = 5; y = 6;      // How to assign values
+x = 5; y = 6;      // How values
 z = x + y;         // How to compute valuesugdjutdjut
